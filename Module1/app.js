704981d5ba160507015a0160d52eee07331768d8
MouadOut <mouadout04@gmail.com>
2024-01-31 13:56:41 +0100

    second

--- Module1/app.js
+++ Module1/app.js
@@ -1,39 +1,38 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('lunchCheck', [])
-.controller('lunchCheckController', lunchCheckController);
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
-lunchCheckController.$inject = ['$scope'];
+    LunchCheckController.$inject = ['$scope'];
 
-function lunchCheckController($scope) {
-    $scope.lunchItems = '';
-    $scope.messageStyle = {'color': 'red'};
-    $scope.textBoxStyle = {'border-color' : 'black'};
-    $scope.checkLunch = function () {
-        var numitems = $scope.lunchItems
-        .split('')
-        .filter(x => x.trim() != '')
-        .length;
+    function LunchCheckController($scope) {
+        $scope.menu = '';
+        $scope.message = '';
+        $scope.messageStyle = '';
 
-        if (numitems ==0) {
-            $scope.message = 'Please Enter Data First';
-            $scope.messageStyle = {'color':'red'};
-            $scope.textBoxStyle = {'border-color':'red'};
+        $scope.getMenu = function() {
+            // Check if the menu is empty
+            if ($scope.menu === '') {
+                $scope.message = 'Please enter data first';
+                $scope.messageStyle = 'red';
+                return;
+            }
+
+            // Split the menu items by comma
+            let menuItemsArray = $scope.menu.split(',');
+
+            // Remove empty items (bonus feature) with trim
+            menuItemsArray = menuItemsArray.filter(item => item.trim() !== '');
+
+            // Check the number of items and set the message accordingly
+            if (menuItemsArray.length <= 3) {
+                $scope.message = 'Enjoy!';
+                $scope.messageStyle = 'green';
+            } else {
+                $scope.message = 'Too much!';
+                $scope.messageStyle = 'green';
+            }
         }
-        else
-        if (numitems <= 3) {
-            $scope.message = 'Enjoy!';
-            $scope.messageStyle = {'color':'green'};
-            $scope.textBoxStyle = {'border-color':'green'};
-        }
-        else { 
-            $scope.message = 'Too much!';
-            $scope.messageStyle = {'color':'green'};
-            $scope.textBoxStyle = {'border-color':'green'};
-        }
-   }
-
-}
- 
+    }
 })();
